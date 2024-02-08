1c6f6554b50af5835fc19499366f49f6b46e2177
YakovBSergeev <154790912+YakovBSergeev@users.noreply.github.com>
2024-02-08 11:05:57 +0600

    error git add_commit_test

---
+++ src/test/java/services/CarServiceImplTest.java
@@ -0,0 +1,24 @@
+package services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarServiceImplTest {
+
+    @Test
+    void takeCarByMark() {
+    }
+
+    @Test
+    void putCar() {
+    }
+
+    @Test
+    void hasCar() {
+    }
+
+    @Test
+    void printCars() {
+    }
+}
