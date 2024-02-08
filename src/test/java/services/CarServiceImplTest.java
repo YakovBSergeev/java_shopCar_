a0a01f5c85b2d700966dec19bd2cceb087b3c557
YakovBSergeev <154790912+YakovBSergeev@users.noreply.github.com>
2024-02-08 16:12:47 +0600

    add_test_method

--- src/test/java/services/CarServiceImplTest.java
+++ src/test/java/services/CarServiceImplTest.java
@@ -1,24 +1,63 @@
 package services;
 
+import domain.Car;
+import lombok.Data;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Data
 class CarServiceImplTest {
+    private final String DEFAULT_MARK = "вольво";
+    private final String DEFAULT_COLOR = "белый";
+    List<Car> carTest = new ArrayList<>();
 
     @Test
     void takeCarByMark() {
+        Car car = new Car( DEFAULT_MARK, DEFAULT_COLOR );
+        carTest.add( car );
+        CarService carService = new CarServiceImpl( carTest );
+        assertFalse( carTest.isEmpty() );
+        carService.takeCarByMark( car.getMark() );
+        assertEquals( DEFAULT_MARK, car.getMark() );
+
     }
 
     @Test
     void putCar() {
+        Car car = new Car( DEFAULT_MARK, DEFAULT_COLOR );
+        carTest.add( car );
+        CarService carService = new CarServiceImpl( carTest );
+        assertFalse( carTest.isEmpty() );
+        carService.putCar( car );
+        assertEquals( DEFAULT_MARK, car.getMark() );
+
+
     }
 
     @Test
     void hasCar() {
+        Car car = new Car( DEFAULT_MARK, DEFAULT_COLOR );
+        carTest.add( car );
+        CarService carService = new CarServiceImpl( carTest );
+        assertFalse( carTest.isEmpty() );
+        assertTrue( carService.hasCar( car.getMark() ) );
+
     }
 
     @Test
     void printCars() {
+        Car car = new Car( DEFAULT_MARK, DEFAULT_COLOR );
+        carTest.add( car );
+        CarService carService = new CarServiceImpl( carTest );
+        assertFalse( carTest.isEmpty() );
+        carService.printCars( );
+        assertEquals( DEFAULT_MARK, car.getMark() );
+        assertEquals( DEFAULT_COLOR, car.getColor() );
+
+
     }
 }
